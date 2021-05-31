f9570cbcd72a4d71fdd483848875703b784d6eb2
Nageswara  Rao <nageswararao@aimlytics.ai>
2021-05-31 20:01:13 +0530

    commit

---
+++ cal.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "e076682f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\"\"\"\n",
+    "Calculator library containing basic math operations.\n",
+    "\"\"\"\n",
+    "\n",
+    "def add(first_term, second_term):\n",
+    "    return first_term + second_term\n",
+    "\n",
+    "def subtract(first_term, second_term):\n",
+    "    return first_term - second_term"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b7fd3f58",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
